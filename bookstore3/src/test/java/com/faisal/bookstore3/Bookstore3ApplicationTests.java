c8bb05c2cba85aebb6e70e3ba6ca6c8efe43fbde
FARHAN <FARHAN@DESKTOP-LEFPGLU>
2020-04-06 15:43:31 -0400

    First Commit - Not include target folder

---
+++ bookstore3/src/test/java/com/faisal/bookstore3/Bookstore3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faisal.bookstore3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bookstore3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
